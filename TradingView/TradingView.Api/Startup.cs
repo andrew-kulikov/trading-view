7c1979d6cc1c43e0e2a7363004f244474fa066da
Andrew <andrew.1.kulikov@gmail.com>
2019-01-27 17:10:50 +0300

    Add bearer authorization

--- TradingView/TradingView.Api/Startup.cs
+++ TradingView/TradingView.Api/Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using TradingView.Api.Providers;
 
 [assembly: OwinStartup(typeof(TradingView.Api.Startup))]
 namespace TradingView.Api
@@ -14,8 +16,29 @@ namespace TradingView.Api
 		public void Configuration(IAppBuilder app)
 		{
 			HttpConfiguration config = new HttpConfiguration();
+
+			ConfigureOAuth(app);
+			
 			WebApiConfig.Register(config);
+			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
 			app.UseWebApi(config);
+		}
+
+
+		public void ConfigureOAuth(IAppBuilder app)
+		{
+			OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+			{
+				AllowInsecureHttp = true,
+				TokenEndpointPath = new PathString("/token"),
+				AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+				Provider = new SimpleAuthorizationServerProvider()
+			};
+
+			// Token Generation
+			app.UseOAuthAuthorizationServer(OAuthServerOptions);
+			app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
 		}
 	}
 }
