b4b89380f52d9d19c6d08c80623f9fdb919875c6
Andrew <andrew.1.kulikov@gmail.com>
2019-01-27 16:03:15 +0300

    Add Startup class

--- TradingView/TradingView.Api/App_Start/WebApiConfig.cs
+++ TradingView/TradingView.Api/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace TradingView.Api
 {
@@ -19,6 +21,9 @@ namespace TradingView.Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-        }
+
+	        var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+	        jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+		}
     }
 }
---
+++ TradingView/TradingView.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TradingView.Api.Startup))]
+namespace TradingView.Api
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			HttpConfiguration config = new HttpConfiguration();
+			WebApiConfig.Register(config);
+			app.UseWebApi(config);
+		}
+	}
+}
