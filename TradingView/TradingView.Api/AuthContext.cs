57cf51d47e34a7ea34f1c6e0badc3a4860dd1e03
Andrew <andrew.1.kulikov@gmail.com>
2019-01-27 16:27:32 +0300

    Add db context and registration

---
+++ TradingView/TradingView.Api/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TradingView.Api
+{
+	public class AuthContext : IdentityDbContext<IdentityUser>
+	{
+		public AuthContext()
+			: base("AuthContext")
+		{
+
+		}
+	}
+}
---
+++ TradingView/TradingView.Api/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TradingView.Api.Models
+{
+	public class ApplicationUser
+	{
+	}
+}
