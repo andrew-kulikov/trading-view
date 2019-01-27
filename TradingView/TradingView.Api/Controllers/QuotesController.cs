4ff372b98d9e7ccea0789353e71622238ed272bc
Andrew <andrew.1.kulikov@gmail.com>
2019-01-27 21:37:16 +0300

    Add frontend application

--- TradingView/TradingView.Api/Controllers/QuotesController.cs
+++ TradingView/TradingView.Api/Controllers/QuotesController.cs
@@ -1,44 +1,57 @@
 ﻿using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace TradingView.Api.Controllers
 {
 	[RoutePrefix("api")]
 	public class QuotesController : ApiController
 	{
-
-		[HttpGet]
 		[Route("config")]
+		[HttpGet]
 		public IHttpActionResult GetConfig()
 		{
-			return Ok(new
+			return Ok(JsonConvert.SerializeObject(new
 			{
 				supported_resolutions = new[] { "1", "5", "15", "30", "60", "1D", "1W", "1M" },
 				supports_group_request = false,
 				supports_marks = false,
 				supports_search = true,
 				supports_timescale_marks = false
-			});
+			}));
 		}
 
-		[HttpGet]
 		[Route("search")]
+		[HttpGet]
 		public IHttpActionResult FindSymbols(
-			[FromUri] string query, [FromUri] string type, 
-			[FromUri] string exchange, [FromUri] string limit)
+			[FromUri] string query = null, [FromUri] string type = null,
+			[FromUri] string exchange = null, [FromUri] string limit = null)
 		{
-			return Ok(new[]
+			return Json(new[]
 			{
 				new Symbol
 				{
-					symbol = "A"
+					symbol = "A",
+					full_name = "A",
+					description = "sdf",
+					exchange = "sdfsdf",
+					type = "kek"
+
 				},
 				new Symbol
 				{
-					symbol = "B"
+					symbol = "B",
+					full_name = "B",
+					description = "sdf",
+					exchange = "sdfsdf",
+					type = "kek"
 				},
 				new Symbol
 				{
-					symbol = "C"
+					symbol = "C",
+					full_name = "C",
+					description = "sdf",
+					exchange = "sdfsdf",
+					type = "kek"
 				},
 			});
 		}
@@ -46,7 +59,7 @@ namespace TradingView.Api.Controllers
 
 	}
 
-	class Symbol
+	internal class Symbol
 	{
 		public string symbol { get; set; }
 		public string description { get; set; }
