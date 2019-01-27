dd40304d718743950b18a2373fa5c070013d8277
Andrew <andrew.1.kulikov@gmail.com>
2019-01-27 20:45:39 +0300

    Add config and search methods

--- TradingView/TradingView.Api/Controllers/QuotesController.cs
+++ TradingView/TradingView.Api/Controllers/QuotesController.cs
@@ -2,14 +2,57 @@
 
 namespace TradingView.Api.Controllers
 {
+	[RoutePrefix("api")]
 	public class QuotesController : ApiController
 	{
-		[Authorize]
-		[Route("api/quotes")]
-		public IHttpActionResult Get()
+
+		[HttpGet]
+		[Route("config")]
+		public IHttpActionResult GetConfig()
 		{
-			return Ok("Quotes");
+			return Ok(new
+			{
+				supported_resolutions = new[] { "1", "5", "15", "30", "60", "1D", "1W", "1M" },
+				supports_group_request = false,
+				supports_marks = false,
+				supports_search = true,
+				supports_timescale_marks = false
+			});
 		}
+
+		[HttpGet]
+		[Route("search")]
+		public IHttpActionResult FindSymbols(
+			[FromUri] string query, [FromUri] string type, 
+			[FromUri] string exchange, [FromUri] string limit)
+		{
+			return Ok(new[]
+			{
+				new Symbol
+				{
+					symbol = "A"
+				},
+				new Symbol
+				{
+					symbol = "B"
+				},
+				new Symbol
+				{
+					symbol = "C"
+				},
+			});
+		}
+
+
+	}
+
+	class Symbol
+	{
+		public string symbol { get; set; }
+		public string description { get; set; }
+		public string exchange { get; set; }
+		public string full_name { get; set; }
+		public string type { get; set; }
 	}
 
 }
