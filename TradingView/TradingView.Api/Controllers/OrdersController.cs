6c2676259db46b1f3b6860709aa031d2428e49b6
Andrew <andrew.1.kulikov@gmail.com>
2019-01-27 20:11:12 +0300

    Remove mock orders

--- TradingView/TradingView.Api/Controllers/OrdersController.cs
+++ TradingView/TradingView.Api/Controllers/OrdersController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace TradingView.Api.Controllers
 {
@@ -11,34 +9,8 @@ namespace TradingView.Api.Controllers
 		[Route("")]
 		public IHttpActionResult Get()
 		{
-			return Ok(Order.CreateOrders());
+			return Ok();
 		}
 	}
-
-	#region Helpers
-
-	public class Order
-	{
-		public int OrderID { get; set; }
-		public string CustomerName { get; set; }
-		public string ShipperCity { get; set; }
-		public Boolean IsShipped { get; set; }
-
-		public static List<Order> CreateOrders()
-		{
-			List<Order> OrderList = new List<Order>
-			{
-				new Order {OrderID = 10248, CustomerName = "Taiseer Joudeh", ShipperCity = "Amman", IsShipped = true },
-				new Order {OrderID = 10249, CustomerName = "Ahmad Hasan", ShipperCity = "Dubai", IsShipped = false},
-				new Order {OrderID = 10250,CustomerName = "Tamer Yaser", ShipperCity = "Jeddah", IsShipped = false },
-				new Order {OrderID = 10251,CustomerName = "Lina Majed", ShipperCity = "Abu Dhabi", IsShipped = false},
-				new Order {OrderID = 10252,CustomerName = "Yasmeen Rami", ShipperCity = "Kuwait", IsShipped = true}
-			};
-
-			return OrderList;
-		}
-	}
-
-	#endregion
 
 }
